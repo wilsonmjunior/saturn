10588c471b207024bc0c649160774e2e6b8893f7
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-01-01 01:36:11 -0300

    feat: config tab navigation

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,53 @@
+import { Tabs } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import { UnistylesTheme } from 'react-native-unistyles';
+
+import { theme } from '../../config/unistyles';
+
+export default function TabsLayout() {
+  return (
+    <UnistylesTheme theme={theme}>
+      <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Inicio",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome
+                size={size}
+                name="home"
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen 
+          name="offers"
+          options={{
+            title: "Ofertas",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome
+                size={size}
+                name="cart-arrow-down"
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen 
+          name="profile"
+          options={{
+            title: "Perfil",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome
+                size={size}
+                name="user"
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </UnistylesTheme>
+  )
+}
