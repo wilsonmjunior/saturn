45148307271322c662da4350d14a723bf777a2d0
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-01-02 18:34:45 -0300

    fix: adjust border radius

--- src/components/Sheet/styles.ts
+++ src/components/Sheet/styles.ts
@@ -15,14 +15,15 @@ export const stylesheet = createStyleSheet((theme) => ({
     position: 'absolute',
     bottom: -SHEET_OVER_DRAG * 1.3,
 
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
   },
   title: {
     color: theme.colors.white,
     fontSize: theme.fontSize.lg,
     fontWeight: 'bold',
-    margin: 24,
+    marginHorizontal: 24,
+    marginVertical: 16,
   },
   dragIcon: {
     marginTop: 16,
