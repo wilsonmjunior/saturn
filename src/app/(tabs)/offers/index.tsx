fbd277421805104334b4596336ddbd44fa3f9980
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-02-04 21:12:50 -0300

    fix: style

--- src/app/(tabs)/offers/index.tsx
+++ src/app/(tabs)/offers/index.tsx
@@ -1,19 +1,19 @@
-import { Link } from 'expo-router'
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import { Button } from '@/components'
+import { Button } from '@/components/common';
+import { View, Text } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export default function Offers () {
   return (
-    <View style={{ flex: 1 }}>
+    <View 
+      style={{ 
+        flex: 1,
+        marginHorizontal: 16,
+        marginTop: getStatusBarHeight() + 32,
+      }}
+    >
       <Text>Ofertas</Text>
-
-      <View style={{ flex: 1, justifyContent: 'flex-end', padding: 16 }}>
-        <Link href="/offers/details" asChild>
-          <Button title="Detalhes" />
-        </Link>
-      </View>
+      
+      <Button title='Detalhes' variant='outline' />
     </View>
   )
 };
