3fcb0b25bfff022e78b722a7a0fc2a18a874ec95
wilson matokanovic junior <wilsonmjuniorx@gmail.com>
2024-03-17 21:04:08 -0300

    fix: add scroll view

--- src/components/screens/Offers/History/index.tsx
+++ src/components/screens/Offers/History/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 import { HistoryCard } from "@/components/common";
 
@@ -7,25 +9,69 @@ export function OffersHistory() {
   const [openHistoryCardId, setOpenHistoryCardId] = useState<number>();
 
   return (
-    <View style={{ gap: 8 }}>
-      <HistoryCard
-        id={1}
-        isOpenActionMenu={openHistoryCardId === 1}
-        onOpenMenu={setOpenHistoryCardId}
-        actionItems={["Editar", "Excluir"]}
-      />
-      <HistoryCard
-        id={2}
-        isOpenActionMenu={openHistoryCardId === 2}
-        onOpenMenu={setOpenHistoryCardId}
-        actionItems={["Editar", "Excluir"]}
-      />
-      <HistoryCard
-        id={3}
-        isOpenActionMenu={openHistoryCardId === 3}
-        onOpenMenu={setOpenHistoryCardId}
-        actionItems={["Editar", "Excluir"]}
-      />
+    <View style={{ flex: 1, paddingBottom: getBottomSpace() + 65 }}>
+      <ScrollView
+        contentContainerStyle={{
+          gap: 16,
+          paddingBottom: getBottomSpace() + 65,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <HistoryCard
+          id={1}
+          isOpenActionMenu={openHistoryCardId === 1}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={2}
+          isOpenActionMenu={openHistoryCardId === 2}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={3}
+          isOpenActionMenu={openHistoryCardId === 3}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={1}
+          isOpenActionMenu={openHistoryCardId === 1}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={2}
+          isOpenActionMenu={openHistoryCardId === 2}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={3}
+          isOpenActionMenu={openHistoryCardId === 3}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={1}
+          isOpenActionMenu={openHistoryCardId === 1}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={2}
+          isOpenActionMenu={openHistoryCardId === 2}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+        <HistoryCard
+          id={3}
+          isOpenActionMenu={openHistoryCardId === 3}
+          onOpenMenu={setOpenHistoryCardId}
+          actionItems={["Editar", "Excluir"]}
+        />
+      </ScrollView>
     </View>
   );
 }
